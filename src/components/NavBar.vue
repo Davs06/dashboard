eeb0276aa0421afb3840a52e9cd9af685ede88ee
Davs06 <santosdv40@gmail.com>
2023-08-09 11:02:16 -0300

    ajuste de identação

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,24 +1,14 @@
 <template>
   <div>
     <v-app-bar>
-      <!-- <v-app-bar-nav-icon>
-        <div class="d-flex">
-          <v-icon icon="/src/assets/desfavelados.jpg"><img
-            src="/src/assets/desfavelados.jpg"
-            
-            class="rounded-circle mr-2"
-          /> </v-icon> <br />
-        </div>
-      </v-app-bar-nav-icon> -->
-
       <v-app-bar-title>
         <div class="d-flex">
-          <v-icon> <img
-            src="/src/assets/logo-desfavelados-removebg-preview.png"
-            height="30"
-            class="rounded-circle mr-3"
+          <v-icon>
+            <img
+              src="/src/assets/logo-desfavelados-removebg-preview.png"
+              height="30"
+              class="rounded-circle mr-3"
           /></v-icon>
-
           <p>Desfavelados</p>
         </div>
       </v-app-bar-title>
@@ -29,11 +19,6 @@
 </template>
 <script>
 export default {
-  methods: {
-    teste() {
-      // console.log(this.$vuetify.themes);
-      console.log("ola");
-    },
-  },
+ 
 };
 </script>
